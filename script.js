10b0d2011e2bbea9fef65653674cfcfafcb24e3c
Isaac Santos <isaac.emanuel@live.com>
2023-12-23 15:36:35 -0600

    add carrusel forms

--- script.js
+++ script.js
@@ -1,75 +1,105 @@
-let currentFromIndex = 0;
-const forms = document.querySelectorAll('.form');
-const formData = [];
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('.btn-add-tech-skill').addEventListener('click', addTechSkill);
+    document.querySelector('.btn-add-soft-skill').addEventListener('click', addSkill); //arregla el tema del btn add
+    document.querySelector('.btn-prev').addEventListener('click', prevForm);
+    document.querySelector('.btn-next').addEventListener('click', nextForm);
+    document.querySelector('.btn-pdf').addEventListener('click', generarPDF);
+    let currentFromIndex = 0;
+    const forms = document.querySelectorAll('.form');
+    const formData = [];
 
-function showForm(index) {
-    console.log(index);
-    forms.forEach((form, i) => {
-        form.classList.toggle('active', i === index);
-    });
-}
-
-function nextForm() {
-    if (currentFromIndex < forms.length - 1) {
-        currentFromIndex++;
-        showForm(currentFromIndex);
+    function showForm(index) {
+        forms.forEach((form, i) => {
+            if (i === index) {
+                form.classList.add('active');
+            } else {
+                form.classList.remove('active');
+            }
+        });
     }
-}
 
-function prevForm() {
-    if (currentFromIndex > 0) {
-        currentFromIndex--;
-        showForm(currentFromIndex);
+    function nextForm() {
+        if (currentFromIndex < forms.length - 1) {
+            currentFromIndex++;
+            saveInfo();
+            showForm(currentFromIndex);
+        }
     }
-}
 
-showForm(currentFromIndex);
-
-function saveInfo() {
-    const currentForm = forms[currentFromIndex];
-    const inputs = currentForm.querySelectorAll('input, textarea');
-
-    const formObject = {};
-    inputs.forEach(input => {
-        formObject[input.name] = input.value;
-    });
-
-    formData[currentFromIndex] = formObject;
-    console.log(formData);
-}
-
-
-
-function addTechSkill() { //add technical skill
-    var techSkillInput = document.getElementById('technicalSkills');
-    var listTechSkills = document.getElementById('listaHabilidadesTecnicas');
-
-    var techSkill = techSkillInput.value.trim();//obtain input value
-
-    if (techSkill !== '') {
-        var newTechSkill = document.createElement('li');
-        newTechSkill.className = 'list-group-item';
-        newTechSkill.textContent = techSkill;
-
-        listTechSkills.appendChild(newTechSkill); //add skill to list
-        techSkillInput.value = '';
+    function prevForm() {
+        if (currentFromIndex > 0) {
+            currentFromIndex--;
+            saveInfo();
+            showForm(currentFromIndex);
+        }
     }
+
+    showForm(currentFromIndex);
+
+    function saveInfo() {
+        const currentForm = forms[currentFromIndex];
+        const inputs = currentForm.querySelectorAll('input, textarea');
+
+        const formObject = {};
+        inputs.forEach(input => {
+            formObject[input.name] = input.value;
+        });
+
+        formData[currentFromIndex] = formObject;
+        console.log(formData);
+    }
+
+
+
+    function addTechSkill() { //add technical skill
+        var techSkillInput = document.getElementById('tech-skills');
+        var listTechSkills = document.getElementById('listaHabilidadesTecnicas');
+
+        var techSkill = techSkillInput.value.trim();//obtain input value
+
+        if (techSkill !== '') {
+            var newTechSkill = document.createElement('li');
+            newTechSkill.className = 'list-group-item';
+            newTechSkill.textContent = techSkill;
+
+            listTechSkills.appendChild(newTechSkill); //add skill to list
+            techSkillInput.value = '';
+        }else {
+            alert('campo incompleto');
+        }
+    }
+
+    function addSkill() { //add soft skill
+        var skillInput = document.getElementById('soft-skills');
+        var listSkills = document.getElementById('listaHabilidades');
+
+        var skill = skillInput.value.trim(); //obtain input value
+
+        if (skill != '') {
+            var newSkill = document.createElement('li');
+            newSkill.className = 'list-group-item';
+            newSkill.textContent = skill;
+
+            listSkills.appendChild(newSkill); //add skill to list
+            skillInput.value = '';
+        } else {
+            alert('campo incompleto');
+        }
+    }
+});
+
+function mostrarVentanaEmergente() { //shows emergent msg
+    document.getElementById('miVentanaEmergente').style.display = 'block';
+    document.getElementById('fondoOscuro').style.display = 'block';
 }
 
-function addSkill() { //add soft skill
-    var skillInput = document.getElementById('softSkills');
-    var listSkills = document.getElementById('listaHabilidades');
+function cerrarVentanaEmergente() { //close emergent msg
+    document.getElementById('miVentanaEmergente').style.display = 'none';
+    document.getElementById('fondoOscuro').style.display = 'none';
+}
 
-    var skill = skillInput.value.trim(); //obtain input value
-
-    if (skill != '') {
-        var newSkill = document.createElement('li');
-        newSkill.className = 'list-group-item';
-        newSkill.textContent = skill;
-
-        listSkills.appendChild(newSkill); //add skill to list
-        skillInput.value = '';
-    }
+function goToForm() { //redirect us to form window
+    window.location.href = "form.html";
 }
 
 function generarPDF() { //generate pdf
@@ -88,26 +118,4 @@ function generarPDF() { //generate pdf
     // Agrega más contenido según sea necesario
 
     pdf.save('curriculum.pdf');
-}
-
-
-function mostrarVentanaEmergente() { //shows emergent msg
-    document.getElementById('miVentanaEmergente').style.display = 'block';
-    document.getElementById('fondoOscuro').style.display = 'block';
-}
-
-function cerrarVentanaEmergente() { //close emergent msg
-    document.getElementById('miVentanaEmergente').style.display = 'none';
-    document.getElementById('fondoOscuro').style.display = 'none';
-}
-
-window.onload = function () { //shows emergent msg inicializing the window
-    const img = document.querySelector('.presentation-background');
-    img.classList.add('popup')
-    //mostrarVentanaEmergente();
-};
-
-
-function goToForm() { //redirect us to form window
-    window.location.href = "form.html";
 }
